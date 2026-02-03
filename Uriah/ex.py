d46a1c9971e8108133c156c1945bd193c40e3e50
Uriah <Uriah.Russell@Outlook.com>
2026-02-03 08:17:01 +0700

    Add Uriah feature branch with greeting function

---
+++ Uriah/ex.py
@@ -0,0 +1,7 @@
+
+
+def thing():
+    x = input("Enter something: ")
+    print(x)
+
+thing()
---
+++ Uriah/greeting.py
@@ -0,0 +1,8 @@
+# greeting.py
+def greet_user():
+    name = input("Enter your name: ")
+    print("Hello, " + name + "!")
+
+if __name__ == "__main__":
+    greet_user()
+    
